afff7644973cc1e28f571cd254df00fa73425d7d
waqasbinhamed <waqasbinhamed@gmail.com>
2020-09-12 18:37:25 +0500

    Added custom 404 and 500 error pages and registration redirects to Topics page.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+
 def register(request):
     """Register a new user."""
     if request.method != 'POST':
@@ -15,7 +16,7 @@ def register(request):
             new_user = form.save()
             # Log the user in and then redireect to home page.
             login(request, new_user)
-            return redirect('learning_logs:index')
+            return redirect('learning_logs:topics')
 
     # Display a blank or invalid form.
     context = {'form': form}
